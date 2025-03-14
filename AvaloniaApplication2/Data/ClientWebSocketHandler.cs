7edcf1eb64a6305fb04eda59239c61749af75859
Your Name <you@example.com>
2025-03-14 15:39:46 +0300

    14.03.2025//First steps

---
+++ AvaloniaApplication2/Data/ClientWebSocketHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Tmds.DBus.Protocol;
+
+namespace AvaloniaApplication2.Data
+{
+    public class ClientWebSocketHandler : IDisposable
+    {
+        public delegate void ClientWebSocketReceiveMessage(string message);
+        public event ClientWebSocketReceiveMessage? OnReceiveMessage;
+
+        private readonly Uri uri = new Uri("ws://217.114.2.102:8088/reviews");
+        private ClientWebSocket webSocket = new ();
+
+        public async Task SendMessages(string message)
+        {
+            await webSocket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(message)),
+                WebSocketMessageType.Text,
+                true,
+                CancellationToken.None);
+        }
+
+        public ClientWebSocketHandler()
+        {
+            _ = InitWebSocket();
+        }
+
+
+        private async Task InitWebSocket()
+        {
+            webSocket = new ClientWebSocket();
+            await webSocket.ConnectAsync(uri, CancellationToken.None);
+            while (webSocket.State == WebSocketState.Open)
+            {
+                ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[1024 * 4]);
+                var result = await webSocket.ReceiveAsync(buffer, CancellationToken.None);
+                OnReceiveMessage?.Invoke(Encoding.UTF8.GetString(buffer.Array, 0, result.Count));
+            }
+        }
+
+        public void Dispose()
+        {
+            webSocket.Dispose();
+        }
+    }
+}
---
+++ AvaloniaApplication2/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AvaloniaApplication2.Data;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AvaloniaApplication2.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        private readonly ClientWebSocketHandler clientWebSocketHandler = new();
+        private string currentMessage = String.Empty;
+
+        public string CurrentMessage
+        {
+            get { return currentMessage;}
+            set
+            {
+                currentMessage = value;
+                OnPropertyChanged(nameof(CurrentMessage));
+            }
+        }
+
+        public ObservableCollection<string> Messages { get; set; } = new();
+
+        public MainWindowViewModel()
+        {
+            clientWebSocketHandler.OnReceiveMessage += DisplayMessage;
+        }
+
+        public async Task SendMessages(string message)
+        {
+            Messages.Add(message);
+            await clientWebSocketHandler.SendMessages(message);
+            CurrentMessage = String.Empty;
+        }
+
+        public void DisplayMessage(string message)
+        {
+            Messages.Add(message);
+        }
+    }
+}
